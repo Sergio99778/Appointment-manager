2a87c375b3c06c66e1ea4090f27df7159a51113b
Sergio99778 <sergio4brandon@gmail.com>
2021-11-29 17:28:25 -0600

    Adding handleChange in Formulario

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,20 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Formulario = () => {
+  //Adding State Appointments
+  const [appointment, setAppointment] = useState({
+    pet: '',
+    owner: '',
+    date: '',
+    time: '',
+    sintoms: '',
+  });
+
+  //Handle Change
+  const handleChange = (e) => {
+    setAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const { pet, owner, date, time, sintoms } = appointment;
   return (
     <Fragment>
       <h2>Create appointment</h2>
       <form action="">
         <label htmlFor="">Pet's name</label>
-        <input type="text" name="pet" className="u-full-width" placeholder="Pet's name" />
+        <input name="pet" className="u-full-width" type="text" placeholder="Pet's name" onChange={handleChange} value={pet} />
         <label htmlFor="">Owner's name</label>
-        <input type="text" name="owner" className="u-full-width" placeholder="Owner's name" />
+        <input type="text" name="owner" className="u-full-width" placeholder="Owner's name" onChange={handleChange} value={owner} />
         <label htmlFor="">Date</label>
-        <input type="date" name="date" className="u-full-width" />
+        <input type="date" name="date" className="u-full-width" onChange={handleChange} value={date} />
         <label htmlFor="">Time</label>
-        <input type="time" name="time" className="u-full-width" />
+        <input type="time" name="time" className="u-full-width" onChange={handleChange} value={time} />
         <label htmlFor="">Sintoms</label>
-        <textarea name="sintoms" className="u-full-width"></textarea>
+        <textarea name="sintoms" className="u-full-width" onChange={handleChange} value={sintoms}></textarea>
         <button type="submit" className="u-full-width button-primary">
           Add appointment
         </button>
